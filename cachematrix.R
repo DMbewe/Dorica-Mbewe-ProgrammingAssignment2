19343a9c7998f7ca08a474e35693304fb23ca3d4
Dorica Mbewe <76525998+DMbewe@users.noreply.github.com>
2021-02-10 22:25:19 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,17 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## Returns
+
+## creates a square matrix and passes it to the cachesolve function
+
+line1 <- c(1,2,1)
+line2 <- c(4,5,2)
+line3 <- c(1,0,1)
+
+x <- rbind(line1,line2,line3)
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## It computes the inverse of a matrix.
+cacheSolve <- function(x) {
+    answer <- solve(x)  
+    print(x)
 }
